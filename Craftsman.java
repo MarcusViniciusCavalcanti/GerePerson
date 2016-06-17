9fcbe1c2a1e4fb4569a26cf3211c0f9686bf4783
Matheus Mazepa <Mazepa@MacBook-Pro-de-Matheus.local>
2016-06-17 15:22:38 -0300

    Cricaco da clase Artesao

---
+++ Craftsman.java
@@ -0,0 +1,315 @@
+/**
+ * Classe Artesão:
+ */ 
+
+public class Craftsman extends Person {
+    private String concept;
+    
+
+    public Craftsman() {
+        super(); //inicializa o construtor da super classe.
+        
+        //define conceito
+        toConcept();
+        
+        //define os atributos.
+        toPrimaryAttributePoints();
+        toSegundaryAttributePoints();
+        toTertiaryAttributePoints();
+        
+        //define as habilidades.
+        toPrimarySkillPoints();
+        toSegundarySkillPoints();
+        toTertiarySkillPoints();
+        
+        //define a distribuição dos pontos bônus.
+        toPointsBonus();
+    }
+
+    /**
+     * Define a distribuição dos pontos dos atributos primária do personagem.
+     * Conceitos de cavaleiros recebem como primária atributos físicos.
+     */
+    public void toPrimaryAttributePoints() {
+        int points = 0;
+        
+         
+        //define quantidade de pontuação baseando na linhagem.
+        if ( this.lineage == 's' )
+          points = 7;
+        else
+          points = 6;
+                  
+        /*
+         * Adiciona os valores dos atributos finaliza o for quando os pontos acabarem.
+         * 0 = força;
+         * 1 = dextreza;
+         * 2 = vigor;
+         */
+        while( points != 0 ){
+            if ( this.physical.get(randomNumber(Physical.SIZE)).addPoints() ) {
+               points--;
+            }
+        }
+    }
+       
+    /**
+     * Define a distribuição dos pontos dos atributos segundária do personagem.
+     * Conceitos de cavaleiros recebem como segundários atributos mentais.
+     */
+    public void toSegundaryAttributePoints() {
+        int points;
+        
+        /*
+         * Define a quantidade de pontos baseados na linhagem
+         * Sobrenatural: 7
+         * Mortal: 6
+         */
+        if( this.lineage == 's' )
+          points = 7;
+        else
+          points = 6;
+          
+        /*
+         * Adiciona de forma aleatória os pontos.
+         * o loop termina quando os pontos acabam.
+         * 
+         * 0 = carisma.
+         * 1 = manipulação.
+         * 2 = aparencia.
+         */
+        while( points != 0 ){
+            if ( this.mental.get(randomNumber(Social.SIZE)).addPoints() ) {
+               points--;
+            }
+        }
+    }
+
+    /**
+     * Define a distribuição dos pontos dos atributos terceária do personagem.
+     * Conceitos de cavaleiros recebem como terceários atributos Sociais.
+     */
+    public void toTertiaryAttributePoints() {
+        int points = 3;
+        
+        /*
+         * Adiciona de forma aleatória os pontos.
+         * o loop termina quando os pontos acabam.
+         * 
+         * 0 = carisma.
+         * 1 = manipulação.
+         * 2 = aparencia.
+         */
+        while( points != 0 ){
+            if ( this.social.get(randomNumber(Mental.SIZE)).addPoints() ) {
+               points--;
+            }
+        }
+        
+    }
+    
+    /**
+     * Define a distribuição dos pontos das Habilidades dos Cavaleiros, num máximo de 3 níveis.
+     * Cavalerios tem como habilidade principal perícia, entretanto eles tende a ter mais afinidade com:
+     *  Cavalgar;
+     *  Empatia com animais;
+     *  Armas brancas e;
+     *  Sobrevivência.  
+     */
+    public void toPrimarySkillPoints() {
+        int points = 0;
+        int aux = 0;
+         
+        //define quantidade de pontuação baseando na linhagem.
+        if ( this.lineage == 's' )
+          points = 13;
+        else
+          points = 11;
+                  
+        /*
+         * Adiciona os valores dos Skill finaliza o for quando os pontos acabarem.
+         * 0 = "Empatia com animais";
+         * 1 = "Arqueirismo";
+         * 2 = "Artesanato"; 
+         * 3 = "Etiqueta";
+         * 4 = "Herborismo"; 
+         * 5 = "Armas brancas",
+         * 6 = "Música";
+         * 7 = "Cavalgar";
+         * 8 = "Furtividade"; 
+         * 9 = "Sobrevivência".
+         */
+        for(int i = 0; i < Expertise.SIZE; i++) {
+           if(i == 0 || i == 5 || i == 7) {
+              if ( this.expertise.get(i).addPoints() ) {
+                 points--;
+              }
+           }
+        }
+        
+        while( points != 0 ){
+            aux = randomNumber(Expertise.SIZE);
+            
+            if ( this.expertise.get(aux).getValue() < 3 )
+              if ( this.expertise.get(aux).addPoints() ) {
+                 points--;
+              }
+        }
+    }
+
+    public void toSegundarySkillPoints() {
+        int points = 0;
+        int aux = 0;
+        
+         
+        //define quantidade de pontuação baseando na linhagem.
+        if ( this.lineage == 's' )
+          points = 9;
+        else
+          points = 7;
+                  
+        /*
+         * Adiciona os valores dos Skill finaliza o for quando os pontos acabarem.
+         * 0 = "Representação";
+         * 1 = "Prontidão";
+         * 2 = "Esportes"; 
+         * 3 = "Briga";
+         * 4 = "Esquiva"; 
+         * 5 = "Empatia",
+         * 6 = "Intimidação";
+         * 7 = "Crime";
+         * 8 = "Liderança"; 
+         * 9 = "Lábia".
+         */
+        for(int i = 0; i < Talent.SIZE; i++) {
+           if(i == 3 || i == 6 || i == 8) {
+              if ( this.talent.get(i).addPoints() ) {
+                 points--;
+              }
+           }
+        }
+        
+        while( points != 0 ){
+            aux = randomNumber(Talent.SIZE);
+            
+            if ( this.talent.get(aux).getValue() < 3 )
+              if ( this.talent.get(aux).addPoints() ) {
+                 points--;
+              }
+        }
+    }
+
+    public void toTertiarySkillPoints() {
+        int points = 0;
+        int aux = 0;
+         
+        //define quantidade de pontuação baseando na linhagem.
+        if ( this.lineage == 's' )
+          points = 5;
+        else
+          points = 4;
+                  
+        /*
+         * Adiciona os valores dos Skill finaliza o for quando os pontos acabarem.
+         * 0 = "Instrução";
+         * 1 = "Sabedoria popular";
+         * 2 = "Investigação"; 
+         * 3 = "Direito";
+         * 4 = "Linguística; 
+         * 5 = "Medicina",
+         * 6 = "Ocultismo";
+         * 7 = "Polícia;
+         * 8 = "Ciência" e; 
+         * 9 = "Senescália".
+         */
+        for(int i = 0; i < Knowledge.SIZE; i++) {
+           if(i == 0 || i == 2 || i == 7) {
+              if ( this.knowledge.get(i).addPoints() ) {
+                 points--;
+              }
+           }
+        }
+        
+        while( points != 0 ){
+            aux = randomNumber(Knowledge.SIZE);
+            
+            if ( this.knowledge.get(aux).getValue() < 3 )
+              if ( this.knowledge.get(aux).addPoints() ) {
+                 points--;
+              }
+        }
+    }
+    
+    public String toString() {
+        int l = 3;
+        int c = 3;
+        String[][] date;
+        date = new String[l][c];
+        String information = "";
+        
+        information = "Nome: " + this.name + " " + this.lastName + "\t\t" + "Conceito: " + this.concept + "\t" 
+                + " Idade: " + this.age + "\n";
+        
+        for(int b = 0; b < l; b++) {
+           date[0][b] = this.physical.get(b).getNameAttribute() + " " + toBall(this.physical.get(b).getValue());
+           date[1][b] = this.social.get(b).getNameAttribute() + " " + toBall(this.social.get(b).getValue());
+           date[2][b] = this.mental.get(b).getNameAttribute() + " " + toBall(this.mental.get(b).getValue());
+        }
+        
+        for(int a = 0; a < c; a++) {
+            for(int b = 0; b < l; b++) 
+               information = information + date[b][a] + "\t";
+            information = information + "\n";
+        }        
+                
+        return information;
+    }
+       
+    /**
+     * Espera um valor int para se transforar em uma String em "ooo--"
+     * retorna esta String.
+     */
+    private String toBall(int n) {
+       String ball = ""; 
+       for (int i = 0; i < n; i++) {
+          ball+= "o";
+       }
+      
+       for (int i = 0; i < (5 - n); i++) {
+          ball+= "-"; 
+       }
+      
+       return ball;
+    }
+    
+    /**
+     * Define o conceito do personagem de forma aleatória.
+     */
+    private void toConcept() {      
+        int i = randomNumber(4);
+        
+        switch ( i ) {
+            case 0:
+                if ( this.gender == 'm' )
+                  this.concept = "Ferreiro";
+                else
+                  this.concept = "Ferreiro";
+                break;
+            case 1:
+                this.concept = "Alfaiate";
+                break;
+            case 2:
+                if ( this.gender == 'm' )
+                  this.concept = "Costureiro";
+                else
+                  this.concept = "Costureira";
+                break;
+            case 3:
+                if ( this.gender == 'm' )
+                  this.concept = "Escultor";
+                else
+                  this.concept = "Escultora";
+                break;
+        }
+    }
+}
