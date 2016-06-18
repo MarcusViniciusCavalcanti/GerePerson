2d343bc22eedfb8f7dc2e24ae1aeed19e78fd312
Matheus Mazepa <Mazepa@MacBook-Pro-de-Matheus.local>
2016-06-18 16:09:25 -0300

    midificacoes classe artist

--- Artist.java
+++ Artist.java
@@ -47,7 +47,7 @@ public class Artist extends Person {
          * 2 = aparencia.
          */
         while( points > 0 ){
-            if ( this.social.get(randomNumber(Social.SIZE)).addPoints() ) {
+            if ( this.social.get(randomNumber(this.social.size())).addPoints() ) {
                points--;
             }
         }          
@@ -78,7 +78,7 @@ public class Artist extends Person {
          * Raciocínio
          */
         while( points > 0 ){
-            if ( this.mental.get(randomNumber(Mental.SIZE)).addPoints() ) {
+            if ( this.mental.get(randomNumber(this.mental.size())).addPoints() ) {
                points--;
             }
         }
@@ -98,7 +98,7 @@ public class Artist extends Person {
          * 2 = vigor;
          */
         while( points > 0 ){
-            if ( this.physical.get(randomNumber(Physical.SIZE)).addPoints() ) {
+            if ( this.physical.get(randomNumber(this.physical.size())).addPoints() ) {
                points--;
             }
         }
@@ -136,7 +136,7 @@ public class Artist extends Person {
          * 9 = "Lábia".
          */
         
-        for(int i = 0; i < Talent.SIZE; i++) {
+        for(int i = 0; i < this.talent.size(); i++) {
            if(i == 0 || i == 5 || i == 9) {
               if ( this.talent.get(i).addPoints() ) {
                  points--;
@@ -145,7 +145,7 @@ public class Artist extends Person {
         }
         
         while( points > 0 ){
-            aux = randomNumber(Expertise.SIZE);
+            aux = randomNumber(this.talent.size());
             
             if ( this.talent.get(aux).getValue() < 3 )
               if ( this.talent.get(aux).addPoints() ) {
@@ -179,7 +179,7 @@ public class Artist extends Person {
          * 8 = "Ciência"; 
          * 9 = "Senescália".
          */
-        for(int i = 0; i < Knowledge.SIZE; i++) {
+        for(int i = 0; i < this.knowledge.size(); i++) {
            if(i == 0 || i == 4) {
               if ( this.knowledge.get(i).addPoints() ) {
                  points--;
@@ -188,7 +188,7 @@ public class Artist extends Person {
         }
         
         while( points > 0 ){
-            aux = randomNumber(Talent.SIZE);
+            aux = randomNumber(this.knowledge.size());
             
             if ( this.knowledge.get(aux).getValue() < 3 )
               if ( this.knowledge.get(aux).addPoints() ) {
@@ -221,7 +221,7 @@ public class Artist extends Person {
          * 8 = "Furtividade"; 
          * 9 = "Sobrevivência".
          */
-        for(int i = 0; i < Expertise.SIZE; i++) {
+        for(int i = 0; i < this.expertise.size(); i++) {
            if(i == 3 || i == 6) {
               if ( this.expertise.get(i).addPoints() ) {
                  points--;
@@ -230,7 +230,7 @@ public class Artist extends Person {
         }
         
         while( points > 0 ){
-            aux = randomNumber(Expertise.SIZE);
+            aux = randomNumber(this.expertise.size());
             
             if ( this.expertise.get(aux).getValue() < 3 )
               if ( this.expertise.get(aux).addPoints() ) {
