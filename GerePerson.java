a08818fa9513f3f94b653ede32e98f105809884f
Matheus Mazepa <Mazepa@MacBook-Pro-de-Matheus.local>
2016-06-19 15:57:33 -0300

    Alteracoes erros em geral

--- GerePerson.java
+++ GerePerson.java
@@ -6,23 +6,38 @@ import java.util.Random;
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class GerePerson
-{   
+public class GerePerson {
     public static void main(String[] args) {  
         Scanner read = new Scanner(System.in);
         Random  rand = new Random();
-      
-        System.out.println("########################################################################");
-        System.out.println("#                                                                      #");
-        System.out.println("#                       Gerador de Personagem                          #");
-        System.out.println("#                                                                      #");
-        System.out.println("########################################################################");
-        System.out.println();
+        
+        String header =      "########################################################################\n"
+                           + "#                                                                      #\n" 
+                           + "#                       Gerador de Personagem                          #\n"
+                           + "#                                                                      #\n"
+                           + "########################################################################";
+        
+        System.out.println(header); 
+        
+        System.out.println("Deseja gerar um arquivo TXT com os dados do personagem? \n Sim[s]\nNão[n]");
+        System.out.print("O que deseja fazer? ");
+        char txtOp = read.next().charAt(0);
+        boolean generateTxt = false;
+        switch (txtOp) {
+            case 's': case 'S':
+                generateTxt = true;
+                break;
+            case 'n': case 'N':
+                generateTxt = false;
+                break;
+            default:
+                System.out.println("Opção inválida!");
+        }
         
         boolean exit = false;
-        while (exit == false) {
+        while (exit == false) {              
             int aux = 0;
-            aux = rand.nextInt(15);
+            aux = rand.nextInt(16);
             Person person = null;
             
             switch (aux) {
@@ -75,74 +90,18 @@ public class GerePerson
                      person = new Foreign();
                      break;
             }
+            System.out.println(header);  
+            System.out.println(person);
             
-            System.out.println("nome: " + person.getName() + " " + person.getLastName() + "\t\t" + "Conceito: "+ person.getConcept() + "\t\t" + "Idade: " + person.getAge());
-            System.out.println();
-        
-            System.out.println("                                   ----------- A T R I B U T O S -----------\n\n"); 
-            System.out.println("     Físicos    \t\t\t Sociais \t\t\t\t Mentais\n");
-            System.out.println("   Força: " + person.getPhysical(0) + "\t\t\t\t" + "    Carisma: " + person.getSocial(0) + "\t\t      " + "   Percepção: " + person.getMental(0));
-            System.out.println("Destreza: " + person.getPhysical(1) + "\t\t\t\t" + "Manipulação: " + person.getSocial(1) + "\t\t      " + "Inteligência: " + person.getMental(1));
-            System.out.println("   Vigor: " + person.getPhysical(2) + "\t\t\t\t" + "  Aparência: " + person.getSocial(2) + "\t\t      " + "  Raciocínio: " + person.getMental(2));
-
-            System.out.println("\n\n                                 ----------- H A B I L I D A D E S -----------\n\n");
-            System.out.println("     Talentos    \t\t\t Perícias \t\t\t\t Conhecimentos\n");
-            System.out.println("Representação: " + person.getTalent(0) + "\t\t" + "Empatia com animais: " + person.getExpertise(0) + "\t\t" + "        Instrução: " + person.getKnowledge(0));
-            System.out.println("    Prontidão: " + person.getTalent(1) + "\t\t" + "        Arqueirismo: " + person.getExpertise(1) + "\t\t" + "Sabedoria popular: " + person.getKnowledge(1));
-            System.out.println("     Esportes: " + person.getTalent(2) + "\t\t" + "         Artesanato: " + person.getExpertise(2) + "\t\t" + "     Investigação: " + person.getKnowledge(2));
-            System.out.println("        Briga: " + person.getTalent(3) + "\t\t" + "           Etiqueta: " + person.getExpertise(3) + "\t\t" + "          Direito: " + person.getKnowledge(3));
-            System.out.println("      Esquiva: " + person.getTalent(4) + "\t\t" + "         Herborismo: " + person.getExpertise(4) + "\t\t" + "      Linguística: " + person.getKnowledge(4));
-            System.out.println("      Empatia: " + person.getTalent(5) + "\t\t" + "      Armas brancas: " + person.getExpertise(5) + "\t\t" + "         Medicina: " + person.getKnowledge(5));            
-            System.out.println("  Intimidação: " + person.getTalent(6) + "\t\t" + "             Música: " + person.getExpertise(6) + "\t\t" + "        Ocultismo: " + person.getKnowledge(6));
-            System.out.println("        Crime: " + person.getTalent(7) + "\t\t" + "           Cavalgar: " + person.getExpertise(7) + "\t\t" + "          Polícia: " + person.getKnowledge(7));
-            System.out.println("    Liderança: " + person.getTalent(8) + "\t\t" + "        Furtividade: " + person.getExpertise(8) + "\t\t" + "          Ciência: " + person.getKnowledge(8));
-            System.out.println("        Lábia: " + person.getTalent(9) + "\t\t" + "      Sobrevivência: " + person.getExpertise(9) + "\t\t" + "       Senescália: " + person.getKnowledge(9));          
-            System.out.println();
-            
-            System.out.println("                        Força de Vontade:");
-            System.out.println("                            " + person.getWillPower());
-            System.out.print("\n\n");
-            
-            System.out.println("deseja gerar um arquivo TXT com os ados do personagem? \n Sim[s]\nNão[n]");
-            System.out.print("O que deseja fazer? ");
-            char txtOp = read.next().charAt(0);
-            String content;
-            switch (txtOp) {
-                case 's': case 'S':
-                    content = "--------------------------- Gerador de Personagem ---------------------------\n" + "\n\n" +
-                                "nome: " + person.getName() + " " + person.getLastName() + "\t\t" + "Conceito: "+ person.getConcept() + "\t\t" + "Idade: " + person.getAge() +"\n" + "\n" +
-                                "                                   ----------- A T R I B U T O S -----------\n\n" + 
-                                "     Físicos   \t\t\tSociais \t\t\t     Mentais\n" + 
-                                "   Força: " + person.getPhysical(0) + "\t\t" + "    Carisma: " + person.getSocial(0) + "\t\t      " + "   Percepção: " + person.getMental(0) + "\n" +
-                                "   Força: " + person.getPhysical(0) + "\t\t" + "    Carisma: " + person.getSocial(0) + "\t\t      " + "   Percepção: " + person.getMental(0) + "\n" +
-                                "Destreza: " + person.getPhysical(1) + "\t\t" + "Manipulação: " + person.getSocial(1) + "\t\t      " + "Inteligência: " + person.getMental(1) + "\n" +
-                                "   Vigor: " + person.getPhysical(2) + "\t\t" + "  Aparência: " + person.getSocial(2) + "\t\t      " + "  Raciocínio: " + person.getMental(2) + "\n" +
-                                "\n\n                                 ----------- H A B I L I D A D E S -----------\n\n" +
-                                "     Talentos    \t\t\t Perícias \t\t\t      Conhecimentos\n" +
-                                "Representação: " + person.getTalent(0) + "\t\t" + "Empatia com animais: " + person.getExpertise(0) + "\t\t" + "        Instrução: " + person.getKnowledge(0) + "\n" +
-                                "    Prontidão: " + person.getTalent(1) + "\t\t" + "        Arqueirismo: " + person.getExpertise(1) + "\t\t" + "Sabedoria popular: " + person.getKnowledge(1) + "\n" +
-                                "     Esportes: " + person.getTalent(2) + "\t\t" + "         Artesanato: " + person.getExpertise(2) + "\t\t" + "     Investigação: " + person.getKnowledge(2) + "\n" +
-                                "        Briga: " + person.getTalent(3) + "\t\t" + "           Etiqueta: " + person.getExpertise(3) + "\t\t" + "          Direito: " + person.getKnowledge(3) + "\n" +
-                                "      Esquiva: " + person.getTalent(4) + "\t\t" + "         Herborismo: " + person.getExpertise(4) + "\t\t" + "      Linguística: " + person.getKnowledge(4) + "\n" +
-                                "      Empatia: " + person.getTalent(5) + "\t\t" + "      Armas brancas: " + person.getExpertise(5) + "\t\t" + "         Medicina: " + person.getKnowledge(5) + "\n" +  
-                                "  Intimidação: " + person.getTalent(6) + "\t\t" + "             Música: " + person.getExpertise(6) + "\t\t" + "        Ocultismo: " + person.getKnowledge(6) + "\n" +
-                                "        Crime: " + person.getTalent(7) + "\t\t" + "           Cavalgar: " + person.getExpertise(7) + "\t\t" + "          Polícia: " + person.getKnowledge(7) + "\n" +
-                                "    Liderança: " + person.getTalent(8) + "\t\t" + "        Furtividade: " + person.getExpertise(8) + "\t\t" + "          Ciência: " + person.getKnowledge(8) + "\n" +
-                                "        Lábia: " + person.getTalent(9) + "\t\t" + "      Sobrevivência: " + person.getExpertise(9) + "\t\t" + "       Senescália: " + person.getKnowledge(9) + "\n" +      
-                                "\n\t\t\t\t\tForça de Vontade:" + "\n" +
-                                "\t\t\t\t\t    " + person.getWillPower() +
-                                "\n\n";
-                    CreateTxt txt = new CreateTxt(person.getName() + " " + person.getLastName(), content);
-                    break;
-                case 'n': case 'N':
-                    exit = true;
-                    break;
-                default:
-                    System.out.println("Opção inválida!");
+            if ( generateTxt ) {
+               String content = person.toString();
+               CreateTxt txt = new CreateTxt(person.getName() + " " + person.getLastName(), content);
+               System.out.println("Gerando personagem com arquivo txt");
             }
+            else
+                System.out.println("Gerando personagem sem arquivo txt");
             
-            System.out.print("\n\n");
-            
+            System.out.println();
             System.out.println("deseja criar outro personagem? \n Sim[s]\n Não[n]");
             System.out.print("O que deseja fazer? ");
             char op = read.next().charAt(0);
@@ -154,10 +113,9 @@ public class GerePerson
                     exit = true;
                     break;
                 default:
-                    System.out.println("Opção inválida!");
+                System.out.println("Opção inválida!");
             }
-            System.out.printf("\u001b");
+            System.out.print("\f");
         }
-        
     }
 }
