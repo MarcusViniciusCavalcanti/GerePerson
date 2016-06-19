56a7b550a2ed7c058e4c546185b952d81a60d7b6
Matheus Mazepa <Mazepa@MacBook-Pro-de-Matheus.local>
2016-06-19 16:51:58 -0300

    Documentacao

--- CreateTxt.java
+++ CreateTxt.java
@@ -3,10 +3,10 @@ import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.io.IOException;
 /**
- * Write a description of class CreatTxt here.
+ * Gera um arquivos .txt com todas as informações do personagem.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Matheus Mazepa e Vinicius Cavalcanti) 
+ * @version (19.06.2016)  
  */
 public class CreateTxt {
     private File path;
@@ -15,13 +15,24 @@ public class CreateTxt {
     private String filePath;
     private String fileName;
     
+    /**
+     * Construtor: Cria a pasta para os arquivos na raiz da aplicação.
+     * Chama o método que gera o nome do arquivo.
+     * Chama o método que escreve dados no arquivo txt.
+     * @param: Nome do arquivo.
+     * @param: Dados do arquivo.
+     */
     public CreateTxt(String fileName, String content) {
-        createPath();           // cria pasta para os arquivos na raiz da aplicação.
-        createFile(fileName);   // gera o nome do arquivo.
-        recordFile(content);    // escreve dados no arquivo txt.
+        createPath();           
+        createFile(fileName);   
+        recordFile(content);    
 
     }
         
+    /**
+     * Método que escreve dados no arquivo txt.
+     * @param: Conteúdo do arquivo .txt.
+     */
     public void recordFile(String content) {
        recordFile = new PrintWriter(file);
        recordFile.printf(content);
@@ -42,20 +53,6 @@ public class CreateTxt {
      * @return: se criado com sucesso verdadeiro falso se não foi gerado.
      */
     private boolean createFile(String fileName) {
-        /*
-         * APAGAR DEPOIS DE ENTENDER.
-         * bloco try catch e como será tratado o erro caso algum problema
-         * aconteça, como estamos lidando com criação de arquivos
-         * o java não termite que algo assim compile sem tratamento
-         * de erro.
-         * try = caso tudo certo faça isso
-         * catch = caso de errao faça iss
-         * Exception = tipo do objeto que vai tratar e uma classa importada
-         * tipo ArrayList 
-         * e = variável to tipo exception que vai manipular
-         * no caso botei um janela de erro caso aconteça isso.
-         */
-
         try {
             file =  new FileWriter("ArquivosTXT" +"/"+ fileName + ".txt");
             return true;
